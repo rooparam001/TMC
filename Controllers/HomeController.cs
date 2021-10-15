5188310f969a583da8b9e8d8d991afcdca63a513
tanuj <tanuj@TK>
2021-10-15 18:10:21 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TMC.Models;
+
+namespace TMC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+            //return View("OnlineTheatreFestival");
+        }
+
+        public IActionResult OnlineTheatreFestival()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ wwwroot/js/loadingcurtain.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+    var viewCount = 0;
+    if (localStorage.getItem("SiteViewCount") === null) {
+        localStorage.setItem('SiteViewCount', 1);
+    }
+    else {
+        viewCount = localStorage.getItem("SiteViewCount");
+        localStorage.removeItem("SiteViewCount");
+        localStorage.setItem('SiteViewCount', ++viewCount);
+
+        if (viewCount <= 2) {
+            localStorage.colorSetting = '#a4509b';
+            $('.main-body-content').hide();
+            $('.loadingcurtain-main').show();
+        }
+        else {
+            $('.loadingcurtain-main').hide();
+            $('.main-body-content').show();
+        }
+    }
+
+
+    $('.loadingcurtain-main').click(function () {
+        $(this).slideUp(1000, 'linear', function () {
+            $('.loadingcurtain-main').hide();
+            $('.main-body-content').show();
+        });
+    });
+});
