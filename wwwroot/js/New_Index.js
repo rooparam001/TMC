a9a3d0fd479e8a3e9f858ce65359bd75a6ab80e6
rooparam001 <rambeniwal151996@gmail.com>
2022-11-19 07:47:36 +0530

    Changes on 19112022

---
+++ wwwroot/js/New_Index.js
@@ -0,0 +1,7 @@
+﻿const wrapper = document.querySelector(".input-wrapper"),
+    textInput = document.querySelector("input[type='text']");
+
+textInput.addEventListener("keyup", event => {
+    wrapper.setAttribute("data-text", event.target.value);
+});
+
